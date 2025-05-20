6f36f77b6d20af9819256d1e8922cd571b635df6
luliucen <liucenlu93@gmail.com>
2025-05-20 17:08:54 +0800

    修改配置文件，安装依赖

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  }
 })
